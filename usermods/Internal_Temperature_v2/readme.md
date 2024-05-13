5b01552dd03889ed188cd7c209ead79e3160bc99
Adam Matthews <adam@wonkatron.com>
2024-05-14 00:08:26 +0100

    Added preset activation
    Added the option to activate a preset when the temperature goes above above the threshold.
    
    Both the preset and the threshold are configurable on the usermod settings page.

--- usermods/Internal_Temperature_v2/readme.md
+++ usermods/Internal_Temperature_v2/readme.md
@@ -1,17 +1,46 @@
 # Internal Temperature Usermod
-This usermod adds the temperature readout to the Info tab and also publishes that over the topic `mcutemp` topic.
 
-## Important
-A shown temp of 53,33°C might indicate that the internal temp is not supported.
+<p align="left">
+  <img width="700" src="assets/screenshot-info.png">
+</p>  
 
-ESP8266 does not have a internal temp sensor
 
-ESP32S2 seems to crash on reading the sensor -> disabled
+## Features
+ - &nbsp;🌡️&nbsp; Adds the internal temperature readout of the chip to the Info tab
+ - 🥵 High temperature indication/action. (configurable preset and threshold)
+ - 📣 Publishes the internal temperature over the MQTT topic: `mcutemp`
+<br><br>
+
+## Use Examples
+- Warn of excessive/damaging temperatures
+- Auto-reduce brightness
+- Activate a cooling fan<br>
+  (Perhaps used in combination with the multi-relay usermod)
+<br><br>
+
+## Compatibility
+- A shown temp of 53,33°C might indicate that the internal temp is not supported
+- ESP8266 does not have a internal temp sensor -> Disabled (Indicated with a readout of '-1')
+- ESP32S2 seems to crash on reading the sensor -> Disabled (Indicated with a readout of '-1')
+<br><br>
 
 ## Installation
-Add a build flag `-D USERMOD_INTERNAL_TEMPERATURE` to your `platformio.ini` (or `platformio_override.ini`).
+- Add a build flag `-D USERMOD_INTERNAL_TEMPERATURE` to your `platformio.ini` (or `platformio_override.ini`).
+<br><br>
+
+## 📝 Change Log
+
+2024-05-11
+
+- Added "high-temperature-indication" feature
+- Documentation updated
+
+2023-09-01
+
+* "Internal Temperature" usermod created
+<br><br>
 
 ## Authors
-Soeren Willrodt [@lost-hope](https://github.com/lost-hope)
-
-Dimitry Zhemkov [@dima-zhemkov](https://github.com/dima-zhemkov)
+- Soeren Willrodt [@lost-hope](https://github.com/lost-hope)
+- Dimitry Zhemkov [@dima-zhemkov](https://github.com/dima-zhemkov)
+- Adam Matthews [@adamsthws](https://github.com/adamsthws)
--- usermods/Internal_Temperature_v2/usermod_internal_temperature.h
+++ usermods/Internal_Temperature_v2/usermod_internal_temperature.h
@@ -10,10 +10,19 @@ private:
   unsigned long lastTime = 0;
   bool isEnabled = false;
   float temperature = 0;
+  float activationThreshold = 90.0;     // Default temperature threshold
+  float resetThreshold = 89.0;          // Reactivation temperature threshold
+  //float reactivationDifference = 1.0; // Reactivation temperature threshold
+  //int lastPreset = 0;                 // Variable to store the last active preset
+  int presetToActivate = 99;            // Default preset setting (-1 = disabled)
+  bool isAboveThreshold = false;        // Flag to track if the high temperature preset is currently active
 
   static const char _name[];
   static const char _enabled[];
   static const char _loopInterval[];
+  static const char _activationThreshold[];
+  static const char _resetThreshold[];
+  static const char _presetToActivate[];
 
   // any private methods should go here (non-inline method should be defined out of class)
   void publishMqtt(const char *state, bool retain = false); // example for publishing MQTT message
@@ -21,6 +30,7 @@ private:
 public:
   void setup()
   {
+  //resetThreshold = activationThreshold - reactivationDifference;
   }
 
   void loop()
@@ -32,6 +42,7 @@ public:
 
     lastTime = millis();
 
+// Measure the temperature
 #ifdef ESP8266 // ESP8266
     // does not seem possible
     temperature = -1;
@@ -40,6 +51,32 @@ public:
 #else                                    // ESP32 ESP32S3 and ESP32C3
     temperature = roundf(temperatureRead() * 10) / 10;
 #endif
+
+    // Check if temperature has gone above the threshold
+    if (temperature >= activationThreshold) {
+      // Update the state flag if not already set
+      if (!isAboveThreshold){
+        isAboveThreshold = true;
+        }
+      // Activate the 'over-threshold' preset if it's not already active
+      if (presetToActivate != -1 && currentPreset != presetToActivate) {
+        //lastPreset = currentPreset;      // Save the current preset to allow re-activation later
+        saveTemporaryPreset();             // Save the current preset to allow re-activation later
+        applyPreset(presetToActivate);     // Activate the high temperature preset
+        }
+      }
+    // Check if temperature is back below the threshold
+    else if (temperature <= resetThreshold) {
+      // Update the state flag if not already set
+      if (isAboveThreshold){
+        isAboveThreshold = false;
+        }
+      // Revert back to the original preset
+      if (currentPreset == presetToActivate){
+        applyTemporaryPreset();
+        //applyPreset(lastPreset);
+        }
+      }
 
 #ifndef WLED_DISABLE_MQTT
     if (WLED_MQTT_CONNECTED)
@@ -56,7 +93,7 @@ public:
     if (!isEnabled)
       return;
 
-    // if "u" object does not exist yet wee need to create it
+    // if "u" object does not exist yet we need to create it
     JsonObject user = root["u"];
     if (user.isNull())
       user = root.createNestedObject("u");
@@ -65,7 +102,7 @@ public:
     userTempArr.add(temperature);
     userTempArr.add(F(" °C"));
 
-    // if "sensor" object does not exist yet wee need to create it
+    // if "sensor" object does not exist yet we need to create it
     JsonObject sensor = root[F("sensor")];
     if (sensor.isNull())
       sensor = root.createNestedObject(F("sensor"));
@@ -80,7 +117,21 @@ public:
     JsonObject top = root.createNestedObject(FPSTR(_name));
     top[FPSTR(_enabled)] = isEnabled;
     top[FPSTR(_loopInterval)] = loopInterval;
+    top[FPSTR(_activationThreshold)] = activationThreshold;
+    top[FPSTR(_resetThreshold)] = resetThreshold;
+    top[FPSTR(_presetToActivate)] = presetToActivate;
   }
+
+    // Append useful info to the usermod settings gui
+    void appendConfigData()
+    {
+    // Display 'ms' next to the 'Loop Interval' setting
+    oappend(SET_F("addInfo('Internal Temperature:Loop Interval', 1, 'ms');"));
+    // Display '°C' next to the 'Activation Threshold' setting
+    oappend(SET_F("addInfo('Internal Temperature:Activation Threshold', 1, '°C');"));
+    // Display '-1 = disabled' next to the 'Preset To Activate' setting
+    oappend(SET_F("addInfo('Internal Temperature:Preset To Activate', 1, '-1 = disabled');"));
+    }
 
   bool readFromConfig(JsonObject &root)
   {
@@ -88,6 +139,15 @@ public:
     bool configComplete = !top.isNull();
     configComplete &= getJsonValue(top[FPSTR(_enabled)], isEnabled);
     configComplete &= getJsonValue(top[FPSTR(_loopInterval)], loopInterval);
+    configComplete &= getJsonValue(top[FPSTR(_presetToActivate)], presetToActivate);
+
+    // Calculate the difference between activation and re-activation thresholds
+    //float oldactivationThreshold = activationThreshold; // Store the old threshold value to compare
+    configComplete &= getJsonValue(top[FPSTR(_activationThreshold)], activationThreshold); // Get activation threshold value
+    configComplete &= getJsonValue(top[FPSTR(_resetThreshold)], resetThreshold);  // Get reset threshold value
+    //if (activationThreshold != oldactivationThreshold) {    // If activation threshold changed...
+    //        resetThreshold = activationThreshold - reactivationDifference;  // Calculate the new re-activation threshold
+    //    }
 
     return configComplete;
   }
@@ -101,6 +161,9 @@ public:
 const char InternalTemperatureUsermod::_name[] PROGMEM = "Internal Temperature";
 const char InternalTemperatureUsermod::_enabled[] PROGMEM = "Enabled";
 const char InternalTemperatureUsermod::_loopInterval[] PROGMEM = "Loop Interval";
+const char InternalTemperatureUsermod::_activationThreshold[] PROGMEM = "Activation Threshold";
+const char InternalTemperatureUsermod::_presetToActivate[] PROGMEM = "Preset To Activate";
+const char InternalTemperatureUsermod::_resetThreshold[] PROGMEM = "Reset Threshold";
 
 void InternalTemperatureUsermod::publishMqtt(const char *state, bool retain)
 {
@@ -115,3 +178,4 @@ void InternalTemperatureUsermod::publishMqtt(const char *state, bool retain)
   }
 #endif
 }
+
